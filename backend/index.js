9f9d7d7311d49f647ba42da87a66947241a8f401
Arin Jain <arinjain1610@gmail.com>
2025-06-30 21:22:02 +0530

    user (register, login, logout, updateProfile) done

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './utils/db.js';
+import userRoute from './routes/user.route.js';
 
 dotenv.config({});
 const app = express();
@@ -13,12 +14,14 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'https//localhost:5173',
+    origin: 'http://localhost:5173',
     credentials: true
 }
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
+
+app.use("/api/v1/user", userRoute);
 
 app.listen(PORT, () => {
     connectDB();
