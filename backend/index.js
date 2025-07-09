10d58c51b0e6de9447ec7592abc632761e7f39d1
Arin Jain <arinjain1610@gmail.com>
2025-07-10 04:35:03 +0530

    url changed

--- backend/index.js
+++ backend/index.js
@@ -20,11 +20,10 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://careersearch.onrender.com/',
     credentials: true
 }
 app.use(cors(corsOptions));
-
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/v1/user", userRoute);
